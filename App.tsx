183fe15807c7ab49f7a7616e1eb355ccae8d681a
Juliettedqb <juliettedqb@gmail.com>
2023-05-10 16:12:16 +0200

    step 2

--- App.tsx
+++ App.tsx
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableNativeFeedback,
+} from "react-native";
 export default function App() {
+  const array = [
+    {
+      title: "Malibu",
+      address: "Malibu 44000",
+      image:
+        "https://www.martysurfdelivery.com/img/leoblog/b/lg-b-aqutaine.jpeg",
+      description: "Regardez comme c'est beau !",
+    },
+    {
+      title: "Hossegor",
+      address: "Hossegor 44000",
+      image:
+        "https://www.martysurfdelivery.com/img/leoblog/b/lg-b-aqutaine.jpeg",
+      description: "Regardez comme c'est beau !",
+    },
+  ];
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {array.map((e) => {
+        return (
+          <View key={e.title}>
+            <TouchableNativeFeedback onPress={() => console.log("pressed")}>
+              <Image
+                source={{
+                  width: 200,
+                  height: 300,
+                  uri: e.image,
+                }}
+              />
+            </TouchableNativeFeedback>
+            <Text>{e.title}</Text>
+          </View>
+        );
+      })}
+
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
