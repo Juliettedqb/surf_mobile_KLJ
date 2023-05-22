f1a2c01b2e7b9554fb558389883b4f88965f4265
Juliettedqb <juliettedqb@gmail.com>
2023-05-22 12:12:53 +0200

    display nearest location

--- App.tsx
+++ App.tsx
@@ -5,10 +5,12 @@ import { Api } from "./ApiController";
 import { convertToCoordinates } from "./utils/convertToCoordinates";
 import { getUserLocation } from "./utils/getLocation";
 import { calculateDistance } from "./utils/calculateDistance";
+import { Button, Text } from "react-native";
 
 export default function App() {
   const [selectedSpot, setSelectedSpot] = useState<SurfData | null>(null);
   const changeSelectedSpot = (spot: SurfData) => setSelectedSpot(spot);
+  const [minDistance, setMinDistance] = useState<string>("");
   const data = Api();
   const fields: SurfData[] = data.map((item) => item._rawJson.fields);
 
@@ -17,27 +19,31 @@ export default function App() {
   );
 
   getUserLocation().then((userLocation) => {
-    const surfDistances = surfItems.map((item, i) => [
-      calculateDistance(
-        userLocation?.latitude,
-        item?.latitude,
-        userLocation?.longitude,
-        item?.longitude
-      ),
-      i,
-    ]);
+    const surfDistances = surfItems.map((item) => {
+      return {
+        distance: calculateDistance(
+          userLocation?.latitude,
+          item?.latitude,
+          userLocation?.longitude,
+          item?.longitude
+        ),
+        name: (item as any).name,
+      };
+    });
+    console.log("distances", surfDistances);
 
     let smallestSumElement = null;
     let smallestSum = Infinity;
 
     for (let i = 0; i < surfDistances.length; i++) {
-      const currentSum = surfDistances[i][0] + surfDistances[i][1];
+      const currentSum = surfDistances[i].distance;
       if (currentSum < smallestSum) {
         smallestSum = currentSum;
         smallestSumElement = surfDistances[i];
       }
     }
 
+    setMinDistance(smallestSumElement?.name);
     console.log("min surfDistance element", smallestSumElement);
   });
 
@@ -47,6 +53,11 @@ export default function App() {
         <Detail onClick={() => setSelectedSpot(null)} item={selectedSpot} />
       ) : (
         <List onClick={changeSelectedSpot} items={fields} />
+      )}
+      {minDistance ? (
+        <Text>Nearest surf spot : {minDistance}</Text>
+      ) : (
+        <Text>Hello</Text>
       )}
     </>
   );
